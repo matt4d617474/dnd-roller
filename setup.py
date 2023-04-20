6d4f0f9a7720a2a1e655e6b2dbe01472d11a6666
Matt Bradshaw <matt.bradshaw.maybe@gmail.com>
2023-04-20 11:30:54 -0500

    Initial commit for dnd-roller v0.0.1

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+from setuptools import find_packages, setup
+
+PKG_FOLDER = Path(os.path.abspath(os.path.dirname(__file__)))
+
+with open(PKG_FOLDER / "README.md") as f:
+    long_description = f.read()
+
+
+setup(
+    name="dnd-roller",
+    version="0.0.1",
+    author="Matt Bradshaw",
+    author_email="matt.bradshaw.maybe@gmail.com",
+    description="Python package to roll D&D dice in the terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/matt4d617474/dnd-roller",
+    include_package_data=True,
+    packages=find_packages(exclude=[]),
+    install_requires=["nomoji==0.0.5", "tabulate"],
+)
+
